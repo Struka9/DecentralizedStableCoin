b4339b4a0e676994a26453ac1f277931df917fd1
Oscar <o_rene89@protonmail.com>
2023-08-10 09:02:36 -0600

    Refactor deploy script

--- script/DeployDecentralizedStableCoin.s.sol
+++ script/DeployDecentralizedStableCoin.s.sol
@@ -10,7 +10,7 @@ contract DeployDecentralizedStableCoin is Script {
     address[] tokenAddresses;
     address[] priceFeedAddresses;
 
-    function run() external returns (DSCEngine, DecentralizedStableCoin) {
+    function run() external returns (DSCEngine, DecentralizedStableCoin, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         (address wethAddress, address wbtcAddress, address wethPriceFeed, address wbtcPriceFeed, uint256 deployerKey) =
             helperConfig.activeNetworkConfig();
@@ -23,6 +23,6 @@ contract DeployDecentralizedStableCoin is Script {
         DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dscInstance));
         dscInstance.transferOwnership(address(engine));
         vm.stopBroadcast();
-        return (engine, dscInstance);
+        return (engine, dscInstance, helperConfig);
     }
 }
